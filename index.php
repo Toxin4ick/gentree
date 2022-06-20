c1b52c7fbf45c6dcaacbf3f32a9b7658b0c21cbf
Toxin <toxin4ick@gmail.com>
2022-06-20 22:35:05 +1000

    сканирование и удаление контейнеров

--- index.php
+++ index.php
@@ -89,6 +89,27 @@ function child($name, &$data)
     }
     return $result;
 }
+function myscandir($dir)
+{
+    $list = scandir($dir);
+    unset($list[0], $list[1]);
+    return array_values($list);
+}
 
+// функция очищения папки
+function clear_dir($dir)
+{
+    $list = myscandir($dir);
+
+    foreach ($list as $file) {
+        if (is_dir($dir . $file)) {
+            clear_dir($dir . $file . '/');
+            rmdir($dir . $file);
+        } else {
+            unlink($dir . $file);
+        }
+    }
+}
 $result = title($data);
+clear_dir('containers/');
 die(json_encode($result, JSON_UNESCAPED_UNICODE));

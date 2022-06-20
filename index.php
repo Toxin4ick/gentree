a1e9f744d34136aaa609db4503ec58ff959175ad
Toxin <toxin4ick@gmail.com>
2022-06-20 22:34:50 +1000

    поиск отношений

--- index.php
+++ index.php
@@ -30,6 +30,47 @@ function title(&$data)
     }
     return $final_result;
 }
+function relation($relation, &$data, $parent)
+{
+    $container = getContainer($relation, $parent);
+    if (empty($container)) {
+        $result = [];
+        $i = 0;
+        foreach ($data as $key=>$row) {
+            if ($row['parent'] == $relation) {
+                $result[$i]['ItemName'] = $row['name'];
+                $result[$i]['parent'] = $relation;
+                $result[$i]['children'] = child($row['name'], $data);
+                $i++;
+            }
+        }
+        write_file($result, $relation);
+        if (!empty($result)) {
+            for ($i = 0; $i < count($result); $i++) {
+                $result[$i]['parent'] = $parent;
+            }
+        }
+        return $result;
+    }
+    return $container;
+}
+
+function getContainer($relation, $parent)
+{
+    $container = @file_get_contents('containers/' . $relation . '.json');
+    $container = json_decode($container, true);
+    if (!empty($container)) {
+        for ($i = 0; $i < count($container); $i++) {
+            $container[$i]['parent'] = $parent;
+        }
+    }
+    return $container;
+}
+function write_file($container, $relation)
+{
+    $file = 'containers/' . $relation . '.json';
+    file_put_contents($file, json_encode($container, JSON_UNESCAPED_UNICODE));
+}
 function child($name, &$data)
 {
     $i = 0;

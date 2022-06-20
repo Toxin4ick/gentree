e8319188db0e9e41867e42a3adb1f6c9cb2c33bb
Toxin <toxin4ick@gmail.com>
2022-06-20 22:34:20 +1000

    поиск детей

--- index.php
+++ index.php
@@ -30,3 +30,24 @@ function title(&$data)
     }
     return $final_result;
 }
+function child($name, &$data)
+{
+    $i = 0;
+    foreach ($data as $key=>$row) {
+        if ($row['parent'] == $name) {
+            $result[$i]['ItemName'] = $row['name'];
+            $result[$i]['parent'] = $row['parent'];
+            if (empty($row['relation'])) {
+                $result[$i]['children'] = child($row['name'], $data);
+                unset($data[$key]);
+            } else {
+                $result[$i]['children'] = relation($row['relation'], $data, $row['name']);
+            }
+            $i++;
+        }
+    }
+    return $result;
+}
+
+$result = title($data);
+die(json_encode($result, JSON_UNESCAPED_UNICODE));
